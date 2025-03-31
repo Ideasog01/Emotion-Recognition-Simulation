27906ae09c9ef318b32299a084a79ddd9e65f48a
Ideasog01 <oliver.dev01@outlook.com>
2025-03-31 17:44:31 +0100

    Eye Tracking Features
    Complete with all eye tracking features, but requires testing

--- Assets/EyeTrackingFeatures.cs
+++ Assets/EyeTrackingFeatures.cs
@@ -8,31 +8,70 @@ public class EyeTrackingFeatures : MonoBehaviour
 {
     public enum FeatureType { Blink, Fixation, Saccade, MicroSaccade }
 
-    private System.Diagnostics.Stopwatch sessionTimer = new System.Diagnostics.Stopwatch();
+    [SerializeField] [ReadOnly] private int sessionNumber = 0;
 
-    private int sessionNumber = 0;
+    [Header("Blink Thresholds")]
+    [SerializeField] private float blinkMinimumDuration = 50; //Minimum duration of blink in milliseconds
+    [SerializeField] private float blinkClosedThreshold = 0.3f; //Threshold for eye closure. 0.0 = closed, 1.0 = open
+    
+    [Header("Fixation Thresholds")]
+    [SerializeField] private float fixationMinimumDuration = 300; //Minimum duration of fixation in milliseconds
+    [SerializeField] private float fixationMaximumAngularVelocity = 20; //Maximum angular velocity for fixation in degrees per second
 
-    private string[] featureDurationArray = new string[3];
-    private string[] featurePerSecondArray = new string[4];
-    private string saccadeDirectionText;
+    [Header("Saccade Thresholds")]
+    [SerializeField] private float saccadeMinimumDuration = 30; //Minimum duration of saccade in milliseconds
+    [SerializeField] private float saccadeMinimumAngularVelocity = 35; //Minimum angular velocity for saccade
+    [SerializeField] private float saccadeMinimumAcceleration = 30; //Minimum acceleration for saccade
+    [SerializeField] private float saccadeMinimumAmplitude = 5; //Minimum amplitude for saccade
 
-    private Vector3 previousGaze;
-    private Vector3 currentGaze;
+    [Header("Micro-Saccade Thresholds")]
+    [SerializeField] private float microSaccadeMinimumDuration = 400; //Minimum duration of micro-saccade in milliseconds
+    [SerializeField] private float microSaccadeMaximumAngularVelocity = 20; //Maximum angular velocity for micro-saccade
+    [SerializeField] private float microSaccadeFixationMinimumDurationThreshold = 400; //Minimum duration of fixation before micro-saccade
 
-    private float previousVelocity;
 
-    private float gazeAngularVelocity = 0;
-    private float gazeAcceleration = 0;
+    private string[] _featureDurationArray = new string[3];
+    private string[] _featurePerSecondArray = new string[4];
 
-    private int[] featureCountPerSecond = { 0, 0}; //Blinks = 0 and Fixations = 1
+    private Vector3 _previousGaze;
+    private Vector3 _currentGaze;
 
-    private List<EyeFeature> eyeFeatures = new List<EyeFeature>();
+    private float _previousVelocity;
+
+    private float _gazeAngularVelocity = 0;
+    private float _gazeAcceleration = 0;
+    private float _saccadeAmplitude = 0;
+
+    private float _microSaccadePeakVelocity = 0;
+
+    private (float, float) _microSaccadeAmplitude = (0, 0);
+
+    private int[] _featureCountPerSecond = { 0, 0, 0, 0}; //Blinks = 0 and Fixations = 1 and Saccade = 2 and Micro-Saccade = 3
+
+    private List<EyeFeature> _eyeFeatures = new List<EyeFeature>();
+
+    private System.Diagnostics.Stopwatch _sessionTimer = new System.Diagnostics.Stopwatch();
 
     private void Update()
     {
-        if(sessionTimer != null && sessionTimer.IsRunning)
+        if(_sessionTimer != null && _sessionTimer.IsRunning)
         {
-            gazeAngularVelocity = GetAngularVelocity();
+            PXR_EyeTracking.GetFoveatedGazeDirection(out Vector3 gazeDirection);
+
+            if(_previousGaze == Vector3.zero)
+            {
+                _previousGaze = gazeDirection.normalized;
+                return;
+            }
+            
+            _currentGaze = gazeDirection.normalized;
+            _gazeAngularVelocity = GetAngularVelocity();
+            _gazeAcceleration = GetAngularVelocity();
+            _saccadeAmplitude = Calculate_saccadeAmplitude();
+            _microSaccadeAmplitude = Calculate_microSaccadeAmplitude();
+
+            _previousGaze = _currentGaze;
+
             TrackEyeTrackingFeatures();
         }
     }
@@ -52,11 +91,14 @@ public class EyeTrackingFeatures : MonoBehaviour
         }
     }
 
+    #region Record Eye Features
+
     private void TrackEyeTrackingFeatures()
     {
-        RecordFeature(FeatureType.Blink, 50);
-        RecordFeature(FeatureType.Fixation, 300);
-        RecordFeature(FeatureType.Saccade, 30);
+        RecordFeature(FeatureType.Blink, blinkMinimumDuration);
+        RecordFeature(FeatureType.Fixation, fixationMinimumDuration);
+        RecordFeature(FeatureType.Saccade, saccadeMinimumDuration);
+        RecordFeature(FeatureType.MicroSaccade, microSaccadeMinimumDuration);
     }
 
     private void RecordFeature(FeatureType type, float minDuration)
@@ -71,19 +113,18 @@ public class EyeTrackingFeatures : MonoBehaviour
                 isFeatureActive = IsEyesClosed(); //Blink detected
                 break;
             case FeatureType.Fixation:
-                isFeatureActive = gazeAngularVelocity < 20; //Fixation detected
+                isFeatureActive = _gazeAngularVelocity <= fixationMaximumAngularVelocity; //Fixation detected
                 break;
             case FeatureType.Saccade:
-                isFeatureActive = gazeAngularVelocity > 35 && gazeAcceleration > 400 && CalculateSaccadeAmplitude() > 5; //Saccade detected
+                isFeatureActive = _gazeAngularVelocity >= saccadeMinimumAngularVelocity && _gazeAcceleration >= saccadeMinimumAcceleration && _saccadeAmplitude >= saccadeMinimumAmplitude; //Saccade detected
                 break;
             case FeatureType.MicroSaccade:
-                
                 EyeFeature fixation = GetEyeFeature(FeatureType.Fixation);
                 EyeFeature saccade = GetEyeFeature(FeatureType.Saccade);
 
                 if(fixation != null)
                 {
-                    isFeatureActive = gazeAngularVelocity < 20 && saccade != null && fixation.featureDuration >= 400; //Micro-Saccade detected
+                    isFeatureActive = _gazeAngularVelocity < microSaccadeMaximumAngularVelocity && saccade != null && fixation.featureDuration >= microSaccadeFixationMinimumDurationThreshold; //Micro-Saccade detected
                 }
                 break;
             default:
@@ -96,11 +137,19 @@ public class EyeTrackingFeatures : MonoBehaviour
             {
                 feature = new EyeFeature(type);
                 feature.featureStartTime = Time.time * 1000; //Convert to milliseconds
-                eyeFeatures.Add(feature);
+                _eyeFeatures.Add(feature);
             }
             else
             {
                 feature.featureDuration = (Time.time * 1000) - feature.featureStartTime;
+
+                if(type == FeatureType.MicroSaccade)
+                {
+                    if(_gazeAngularVelocity > _microSaccadePeakVelocity)
+                    {
+                        _microSaccadePeakVelocity = _gazeAngularVelocity; //Record the peak velocity of the micro-saccade
+                    }
+                }
             }
         }
         else
@@ -109,76 +158,66 @@ public class EyeTrackingFeatures : MonoBehaviour
             {
                 feature.featureDuration = (Time.time * 1000) - feature.featureStartTime;
 
-                if(feature.featureDuration > minDuration) //Valid
+                if(feature.featureDuration >= minDuration) //Valid
                 {
-                    if(feature.featureDuration < 1000) // Happened this second
+                    if(feature.featureDuration <= 1000) // Happened this second
                     {
-                        featureCountPerSecond[(int)type]++;
+                        _featureCountPerSecond[(int)type]++;
                     }
 
                     //Assign to string for writing to file at end of session
-                    featureDurationArray[(int)type] += feature.featureDuration.ToString() + "\n"; //Record the duration of the feature
+                    _featureDurationArray[(int)type] += feature.featureDuration.ToString() + "\n"; //Record the duration of the feature
 
-                    //Assign to string for Saccade direction (if applicable)
                     if(type == FeatureType.Saccade)
                     {
-                        PXR_EyeTracking.GetFoveatedGazeDirection(out Vector3 gazeDirection);
-                        gazeDirection.Normalize();
-
-                        Vector3 previousDirection = new Vector3(previousGaze.x, 0, previousGaze.z).normalized;
-                        Vector3 currentDirection = new Vector3(currentGaze.x, 0, currentGaze.z).normalized;
-
-                        float saccadeAngle = Vector3.SignedAngle(previousDirection, currentDirection, Vector3.up);
-
-                        if(saccadeAngle < 0)
-                        {
-                            saccadeAngle += 360; //Convert to positive angle;
-                        }
-
-                        saccadeDirectionText += saccadeAngle.ToString() + "\n"; //Record the angle of the saccade
+                        WriteSaccadeFeatures();
                     }
+                    else if(type == FeatureType.MicroSaccade)
+                    {
+                        WriteMicroSaccadeFeatures();
+                    }
+
                 }
                 
-                eyeFeatures.Remove(feature); //Blink was too short, remove it
+                _eyeFeatures.Remove(feature); //Blink was too short, remove it
             }
         }
     }
 
-    #region Eye Properties
+    #endregion
 
-    private float GetAngularVelocity()
+    #region  Saccade and Micro-Saccade Features
+
+    private string saccadeDirectionText = "";
+
+    private void WriteSaccadeFeatures()
     {
-        PXR_EyeTracking.GetFoveatedGazeDirection(out Vector3 gazeDirection);
-        currentGaze = gazeDirection.normalized;
-
-        float angleVelocity = 0;
-        float deltaTime = Time.deltaTime;
-
-        if (previousGaze != Vector3.zero && deltaTime > 0) 
-        {
-            float dotProduct = Vector3.Dot(previousGaze, currentGaze);
-            dotProduct = Mathf.Clamp(dotProduct, -1.0f, 1.0f);
-
-            float angleRadians = Mathf.Acos(dotProduct);
-            float angleDegrees = Mathf.Rad2Deg * angleRadians;
-
-            angleVelocity = angleDegrees / deltaTime; // Convert to velocity
-        }
-
-        //Compute acceleration
-        gazeAcceleration = (angleVelocity - previousVelocity) / deltaTime;
-        previousVelocity = angleVelocity;
-
-        previousGaze = currentGaze;
-        return angleVelocity;
+        saccadeDirectionText += _currentGaze.ToString() + "\n"; //Record the direction of the saccade
     }
+
+    private string _microSaccadePeakVelocityText = "";
+    private string microSaccadeDirectionText = "";
+    private string microSaccadeHorizontalAmplitudeText = "";
+    private string microSaccadeVerticalAmplitudeText = "";
+
+    private void WriteMicroSaccadeFeatures()
+    {
+        _microSaccadePeakVelocityText += _microSaccadePeakVelocity.ToString() + "\n"; //Record the peak velocity of the micro-saccade
+        microSaccadeDirectionText += _currentGaze.ToString() + "\n"; //Record the direction of the micro-saccade
+        microSaccadeHorizontalAmplitudeText += _microSaccadeAmplitude.Item1.ToString() + "\n"; //Record the horizontal amplitude of the micro-saccade
+        microSaccadeVerticalAmplitudeText += _microSaccadeAmplitude.Item2.ToString() + "\n"; //Record the vertical amplitude of the micro-saccade
+    }
+
+    #endregion
+
+    #region Eye Properties
 
     private bool IsEyesClosed()
     {
         PXR_EyeTracking.GetLeftEyeGazeOpenness(out float leftEyeOpenness);
         PXR_EyeTracking.GetRightEyeGazeOpenness(out float rightEyeOpenness);
 
-        if(leftEyeOpenness < 0.25f && rightEyeOpenness < 0.25f) //Eye closed detected, check if min duration matches 50ms
+        if(leftEyeOpenness <= blinkClosedThreshold && rightEyeOpenness <= blinkClosedThreshold) //Eye closed detected, check if min duration matches 50ms
         {
             return true;
         }
@@ -188,12 +227,32 @@ public class EyeTrackingFeatures : MonoBehaviour
         }
     }
 
-    private float CalculateSaccadeAmplitude()
+    private float GetAngularVelocity()
     {
-        PXR_EyeTracking.GetFoveatedGazeDirection(out Vector3 gazeDirection);
-        currentGaze = gazeDirection.normalized;
+        float angleVelocity = 0;
+        float deltaTime = Time.deltaTime;
 
-        float dotProduct = Vector3.Dot(previousGaze, currentGaze);
+        if (_previousGaze != Vector3.zero && deltaTime > 0) 
+        {
+            float dotProduct = Vector3.Dot(_previousGaze.normalized, _currentGaze.normalized);
+            dotProduct = Mathf.Clamp(dotProduct, -1.0f, 1.0f);
+
+            float angleRadians = Mathf.Acos(dotProduct);
+            float angleDegrees = Mathf.Rad2Deg * angleRadians;
+
+            angleVelocity = angleDegrees / deltaTime; // Convert to velocity
+        }
+
+        //Compute acceleration
+        _gazeAcceleration = (angleVelocity - _previousVelocity) / deltaTime;
+        _previousVelocity = angleVelocity;
+
+        return angleVelocity;
+    }
+
+    private float Calculate_saccadeAmplitude()
+    {
+        float dotProduct = Vector3.Dot(_previousGaze.normalized, _currentGaze.normalized);
         dotProduct = Mathf.Clamp(dotProduct, -1.0f, 1.0f);
 
         float angleRadians = Mathf.Acos(dotProduct);
@@ -202,10 +261,24 @@ public class EyeTrackingFeatures : MonoBehaviour
         return angleDegrees;
     }
 
+    private (float horizontalAmplitude, float verticalAmplitude) Calculate_microSaccadeAmplitude()
+    {
+        Vector2 start2D = new Vector2(_previousGaze.x, _previousGaze.y).normalized;
+        Vector2 end2D = new Vector2(_currentGaze.x, _currentGaze.y).normalized;
+
+        float horizontalDiff = Mathf.Atan2(end2D.x, 1) - Mathf.Atan2(start2D.x, 1);
+        float verticalDiff = Mathf.Atan2(end2D.y, 1) - Mathf.Atan2(start2D.y, 1);
+
+        float horizontalAmplitude = Mathf.Abs(Mathf.Rad2Deg * horizontalDiff);
+        float verticalAmplitude = Mathf.Abs(Mathf.Rad2Deg * verticalDiff);
+
+        return (horizontalAmplitude, verticalAmplitude);
+    }
+
 
     private EyeFeature GetEyeFeature(FeatureType featureType)
     {
-        foreach(EyeFeature feature in eyeFeatures)
+        foreach(EyeFeature feature in _eyeFeatures)
         {
             if(feature.featureType == featureType)
             {
@@ -222,14 +295,27 @@ public class EyeTrackingFeatures : MonoBehaviour
 
     private void StartSession()
     {
-        sessionTimer.Start();
-        sessionNumber++;
-
+        _sessionTimer.Reset();
         for(int i = 0; i < 2; i++)
         {
-            featureDurationArray[i] = "";
-            featureCountPerSecond[i] = 0;
-        }   
+            _featureDurationArray[i] = "";
+            _featureCountPerSecond[i] = 0;
+        }
+
+        _microSaccadePeakVelocityText = "";
+        _microSaccadePeakVelocity = 0;
+        _previousVelocity = 0;
+        microSaccadeDirectionText = "";
+        microSaccadeHorizontalAmplitudeText = "";
+        microSaccadeVerticalAmplitudeText = "";
+
+        saccadeDirectionText = "";
+
+        _previousGaze = Vector3.zero;
+        _currentGaze = Vector3.zero;
+
+        _sessionTimer.Start();
+        sessionNumber++;
 
         StartCoroutine(PerSecondTimer());
     }
@@ -237,16 +323,26 @@ public class EyeTrackingFeatures : MonoBehaviour
     private void EndSession()
     {
         //Write data to files
-        sessionTimer.Stop();
-        float sessionDuration = (float)sessionTimer.Elapsed.TotalSeconds; //Total time of session in seconds
+        _sessionTimer.Stop();
+        float sessionDuration = (float)_sessionTimer.Elapsed.TotalSeconds; //Total time of session in seconds
         
         WriteFile(sessionDuration.ToString(), "SessionDuration");
 
-        WriteFile(featureDurationArray[(int)FeatureType.Blink], "BlinksDuration");
-        WriteFile(featurePerSecondArray[(int)FeatureType.Blink], "BlinksPerSecond");
+        WriteFile(_featureDurationArray[(int)FeatureType.Fixation], "FixationsDuration");
+        WriteFile(_featurePerSecondArray[(int)FeatureType.Fixation], "FixationsPerSecond");
+        
+        WriteFile(_featurePerSecondArray[(int)FeatureType.MicroSaccade], "MicroSaccadesPerSecond");
+        WriteFile(_microSaccadePeakVelocityText, "_microSaccadePeakVelocity");
+        WriteFile(microSaccadeDirectionText, "MicroSaccadeDirection");
+        WriteFile(microSaccadeHorizontalAmplitudeText, "MicroSaccadeHorizontalAmplitude");
+        WriteFile(microSaccadeVerticalAmplitudeText, "MicroSaccadeVerticalAmplitude");
 
-        WriteFile(featureDurationArray[(int)FeatureType.Fixation], "FixationsDuration");
-        WriteFile(featurePerSecondArray[(int)FeatureType.Fixation], "FixationsPerSecond");
+        WriteFile(_featurePerSecondArray[(int)FeatureType.Saccade], "SaccadesPerSecond");
+        WriteFile(_featureDurationArray[(int)FeatureType.Saccade], "SaccadesDuration");
+        WriteFile(saccadeDirectionText, "SaccadeDirection");
+
+        WriteFile(_featurePerSecondArray[(int)FeatureType.Blink], "BlinksPerSecond");
+        WriteFile(_featureDurationArray[(int)FeatureType.Blink], "BlinksDuration");
     }
 
     private IEnumerator PerSecondTimer() //For recording events every second that the session is active
@@ -255,11 +351,11 @@ public class EyeTrackingFeatures : MonoBehaviour
 
         for(int i = 0; i < 4; i++)
         {
-            featurePerSecondArray[i] += featureCountPerSecond[0].ToString() + "\n";
-            featureCountPerSecond[i] = 0;
+            _featurePerSecondArray[i] += _featureCountPerSecond[0].ToString() + "\n";
+            _featureCountPerSecond[i] = 0;
         }
 
-        if(sessionTimer.IsRunning)
+        if(_sessionTimer.IsRunning)
         {
             StartCoroutine(PerSecondTimer());
         }
---
+++ Assets/Scripts/ReadOnlyAttribute.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEditor;
+using Unity.Properties;
+
+public class ReadOnlyAttribute : PropertyAttribute
+{
+    //Serves as a marker
+}
